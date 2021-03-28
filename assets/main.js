d938f2162e5a00d7f4b22a085a34e4689400a2bd
giuseppe <placidagiuseppe@gmail.com>
2021-03-28 17:33:32 +0200

    Milestone 3 completata

--- assets/main.js
+++ assets/main.js
@@ -2,30 +2,58 @@ var app = new Vue({
     el:'#root',
     data:{
       contacts: contacts,
+       user: [],
+      input:" ",
+    },
 
+     methods: {
+       click_utente: function (contact){
+           this.user = contact,
+           console.log(this.user);
+       },
+       //aggiunta di un messaggio nella chat con data e risposta con set timeour
+       add: function (user) {
+   //creo date per prendermi l'ora attuale
+         var date = new Date();
+         var ora = date.getHours();
+         var minuti = date.getMinutes();
+         var mese = date.getMonth() +1;
+         var secondi = date.getSeconds();
+         var anno = date.getFullYear();
+         var giorno = date.getDate();
+//concateno le stringhe per ottenere la data totale
+         var data_tot =`${giorno}/${mese}/${anno} ${ora}:${minuti}:${secondi}`;
+         // giorno+"/"+mese+"/"+ anno +" " +ora+":"+ minuti+":" + secondi;
+         console.log(data_tot);
+
+         if (this.input != " ") {
+           let aggiunta = {
+            date:data_tot,
+            text: this.input,
+            status:'sent',
+            }
+
+            user.messages.push(aggiunta);
+            console.log(this.user.messages);
+            this.input = " ";
+
+            setTimeout(function(){
+              //ricreo data per prendermi il il secondo attuale
+              var data_second = new Date();
+              var secondi = data_second.getSeconds();
+              var data_tot =`${giorno}/${mese}/${anno} ${ora}:${minuti}:${secondi}`;
+              let ok = {
+                date:data_tot,
+                text: "ok",
+                status:'received',
+              }
+              //aggiungo messaggio automatico
+              user.messages.push(ok);
+            }, 1000);
+
+        }
+
+      },
     }
-    // created:function(){
-    //       obj:{
-    //     ...contacts.messages[text],
-    //   },
-    //   console.log(obj);
-    // },
-
-    // methods:{
-    //   // console.log(this.contacts);
-    //   obj:function () {
-    //     this.contact.forEach((item, i) => {
-    //       let oggetto = {...this.contacts}
-    //       console.log(oggetto.text);
-    //     });
-    //
-    //
-    //   },
-    //
-    // },
-    // created:function () {
-    //   this.obj();
-    //
-    // }
 
 });
